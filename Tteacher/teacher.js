85f4c0b23fb10a128810deb0473f3d063d5b0d72
juyanglin <49740728+juyanglin@users.noreply.github.com>
2023-09-22 09:46:21 +0800

    v2

--- Tteacher/teacher.js
+++ Tteacher/teacher.js
@@ -50,63 +50,75 @@ function link( )
         } 
 		 function link11( )
 		{
-            alert('呂琬琪')
+            alert('林淑妙')
         } 
 		 function link13( )
 		{
-            alert('王巧沛')
+            alert('陳靜卿')
         } 
          function link21( )
 		{
-            alert('林淑妙')
+            alert('張麗美')
         } 
 		 function link23( )
 		{
-            alert('吳美潔')
+            alert('陳舜明')
         } 
 		 function link31( )
 		{
-            alert('陳靜卿')
+            alert('簡郁雅')
         } 
 		 function link33( )
 		{
-            alert('簡郁雅')
+            alert('郭雅萍')
         } 
 		  function link41( )
 		{
-            alert('林士傑')
+            alert('涂大節')
         } 
 		 function link43( )
 		{
-            alert('葉建宏')
+            alert('吳美潔')
         } 
 		 function link51( )
 		{
-            alert('陳舜明')
+            alert('蔡旻家')
         } 
 		 function link52( )
 		{
-            alert('涂大節')
+            alert('邱渝婷')
         } 
 		
 		 function link61( )
 		{
-            alert('張麗美')
+            alert('林士傑')
         } 
 		 function link62( )
 		{
-            alert('蔡旻家')
+            alert('葉建宏')
         } 
 		 function link63( )
 		{
-            alert('葉建宏')
+            alert('')
         } 
 		
         function link34( )
 		{
-            alert('張寶月,闕妤甄')
+            alert('劉瑞德,闕妤甄')
         } 
 		 function link100( )
 		{
             alert('何月娥,李佩瑾,')
         } 
+        function link82( )
+		{
+            alert('呂琬琪')
+        }
+        function link81( )
+		{
+            alert('陳冠伶')
+        }
+        function link80( )
+		{
+            alert('郭馨鎂')
+        }
